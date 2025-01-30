8edf41ab37d478761aa0ace46f146aebb1c2d54b
Matt Jarvis <mattj23@gmail.com>
2025-01-30 14:13:19 -0500

    matplotlib tools

--- python/engeom/pyvista.py
+++ python/engeom/pyvista.py
@@ -7,20 +7,20 @@ from .engeom import Mesh
 try:
     import pyvista
 except ImportError:
-    pyvista = None
+    pass
+else:
 
+    def mesh_polydata(mesh: Mesh) -> pyvista.PolyData:
+        """
+        Creates a PyVista PolyData object from a Mesh object.
+        :param mesh: a Mesh object
+        :return: a PyVista PolyData object
+        """
 
-def mesh_polydata(mesh: Mesh) -> "pyvista.PolyData":
-    """
-    Creates a PyVista PolyData object from a Mesh object.
-    :param mesh: a Mesh object
-    :return: a PyVista PolyData object
-    """
+        if pyvista is None:
+            raise ImportError("PyVista is not installed.")
 
-    if pyvista is None:
-        raise ImportError("PyVista is not installed.")
-
-    vertices = mesh.clone_vertices()
-    faces = mesh.clone_triangles()
-    faces = numpy.hstack((numpy.ones((faces.shape[0], 1), dtype=faces.dtype) * 3, faces))
-    return pyvista.PolyData(vertices, faces)
+        vertices = mesh.clone_vertices()
+        faces = mesh.clone_triangles()
+        faces = numpy.hstack((numpy.ones((faces.shape[0], 1), dtype=faces.dtype) * 3, faces))
+        return pyvista.PolyData(vertices, faces)
