52daacae4d37327910ec8c54c699abd0cbbe707f
Matt Jarvis <mattj23@gmail.com>
2025-02-09 23:24:20 -0500

    small changes to mesh

--- python/engeom/geom3.pyi
+++ python/engeom/geom3.pyi
@@ -434,7 +434,7 @@ class Mesh:
     def __init__(
             self,
             vertices: numpy.ndarray[float],
-            triangles: numpy.ndarray[numpy.uint32],
+            faces: numpy.ndarray[numpy.uint32],
             merge_duplicates: bool = False,
             delete_degenerate: bool = False
     ):
@@ -445,7 +445,7 @@ class Mesh:
         front/outside.
 
         :param vertices: a numpy array of shape (n, 3) containing the vertices of the mesh.
-        :param triangles: a numpy array of shape (m, 3) containing the triangles of the mesh, should be uint.
+        :param faces: a numpy array of shape (m, 3) containing the triangles of the mesh, should be uint.
         :param merge_duplicates: merge duplicate vertices and triangles
         :param delete_degenerate: delete degenerate triangles
         """
@@ -506,7 +506,7 @@ class Mesh:
         ...
 
     @property
-    def points(self) -> numpy.ndarray[float]:
+    def vertices(self) -> numpy.ndarray[float]:
         """
         Will return an immutable view of the vertices of the mesh as a numpy array of shape (n, 3).
         :return: a numpy array of shape (n, 3) containing the vertices of the mesh.
@@ -514,7 +514,7 @@ class Mesh:
         ...
 
     @property
-    def triangles(self) -> numpy.ndarray[numpy.uint32]:
+    def faces(self) -> numpy.ndarray[numpy.uint32]:
         """
         Will return an immutable view of the triangles of the mesh as a numpy array of shape (m, 3).
         :return: a numpy array of shape (m, 3) containing the triangles of the mesh.
